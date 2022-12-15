23bf0107a3eb036bc6055e8a191d239f781e46da
Dez <dezd24@gmail.com>
2022-12-15 14:01:36 -0800

    JS

--- script.js
+++ script.js
@@ -14,9 +14,6 @@ console.log(searchHistory);
 
 // VAR - CONSTANTS
 const apiKey = "192233ca5c2f748da450fa07039ba6ca";
-const searchBtn = document.querySelector("#searchBtn");
-const searchInput = document.querySelector("#searchInput");
-const searchBtn = document.querySelector("#searchBtn");
 const searchInput = document.querySelector("#searchInput");
 const searchBtn = document.querySelector("#searchBtn");
 
@@ -25,45 +22,46 @@ searchBtn.addEventListener("click", function apiKey() {
     const user = document.querySelector("#searchInput").value;
     function storeClicks() {
         storeQuery()
-history.addEventListener("click", searchHistory);
-searchWeather. addEventListener("click", searchWeather);
+        history.addEventListener("click", searchHistory);
+        searchWeather.addEventListener("click", searchWeather);
 
-// FUNCTIONS
-function searchBtn() {
-    
-function searchInput() {
-}
+        // FUNCTIONS
+        function searchBtn() {
 
-function getCurrentWeather() {
-    if (let i = 0; i < searchHistory.length; i++) {
-        localStorage.getItem("searchHistory");
-        
-       
-function getForecast() {
-    if (let i = 0; i < searchHistory.length; i++) {
-        localStorage.getItem("searchHistory");
-        
+            function searchInput() {
+            }
 
-function searchHistory() {
-  
-/*GET LAT AND LONG OF CITIES from THE WEATHER DATA on 97*/
-function searchWeather() {
-    const cityName = searchInput.value;
-    const apiURL = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=5&appid="+apiKey;
-    fetch(apiURL)
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data) {
-        console.log(data);
+            function getCurrentWeather() {
+                if (let i = 0; i < searchHistory.length; i++) {
+    localStorage.getItem("searchHistory");
 
-    })
-}
 
-// LOCAL STORAGE
-localStorage.setItem("search", JSON.stringify(searchHistory));
+    function getForecast() {
+        if (let i = 0; i < searchHistory.length; i++) {
+            localStorage.getItem("searchHistory");
 
-// CALLS
-searchHistory();
+
+            function searchHistory() {
+
+                /*GET LAT AND LONG OF CITIES from THE WEATHER DATA on 97*/
+                //this is saying  that  we want to  search the weather using the city name we will connect to the open weather  api and the  fetch (a promise) will return the information/
+                function searchWeather() {
+                    const cityName = searchInput.value;
+                    const apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
+                    fetch(apiURL)
+                        .then(function (response) {
+                            return response.json()
+                        })
+                        .then(function (data) {
+                            console.log(data);
+
+                        })
+                }
+
+                // LOCAL STORAGE
+                localStorage.setItem("search", JSON.stringify(searchHistory));
+
+                // CALLS
+                searchHistory();
 
 
