ebc7863a5861e6eaf7ed0621d71f777b8f311958
Dez <dezd24@gmail.com>
2022-12-15 18:11:10 -0800

    JS and events

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// API KEY
-var apiKey = "192233ca5c2f748da450fa07039ba6ca";
+// // API KEY
+// var apiKey = "192233ca5c2f748da450fa07039ba6ca";
 
 // HOOKS
-var searchBtn = document.querySelector("#searchBtn");
-var searchInput = document.querySelector("#searchInput");
+// var searchBtn = document.querySelector("#searchBtn");
+var searchInput = document.querySelector("#search-input");
 var currentWeather = document.querySelector("#currentWeather");
 var forecast = document.querySelector("#forecast");
 var history = document.querySelector("#history");
@@ -14,54 +14,58 @@ console.log(searchHistory);
 
 // VAR - CONSTANTS
 const apiKey = "192233ca5c2f748da450fa07039ba6ca";
-const searchInput = document.querySelector("#searchInput");
-const searchBtn = document.querySelector("#searchBtn");
+// const searchInput = document.querySelector("#searchInput");
+const searchBtn = document.querySelector("#search-button");
 
 // EVENT LISTENERS
-searchBtn.addEventListener("click", function apiKey() {
-    const user = document.querySelector("#searchInput").value;
-    function storeClicks() {
-        storeQuery()
-        history.addEventListener("click", searchHistory);
-        searchWeather.addEventListener("click", searchWeather);
+searchBtn.addEventListener("click", searchWeather);
 
-        // FUNCTIONS
-        function searchBtn() {
+// const user = document.querySelector("#searchInput").value;
+// function storeClicks() {
+//     storeQuery()
+//     history.addEventListener("click", searchHistory);
+//     searchWeather.addEventListener("click", searchWeather);
 
-            function searchInput() {
-            }
+//     // FUNCTIONS
+//     function searchBtn() {
 
-            function getCurrentWeather() {
-                if (let i = 0; i < searchHistory.length; i++) {
-    localStorage.getItem("searchHistory");
+//         function searchInput() {
+//         }
+
+//         function getCurrentWeather() {
+//             if (let i = 0; i < searchHistory.length; i++) {
+// // localStorage.getItem("searchHistory");
+// 
+
+// function getForecast() {
+//     if (let i = 0; i < searchHistory.length; i++) {
+//         localStorage.getItem("searchHistory");
 
 
-    function getForecast() {
-        if (let i = 0; i < searchHistory.length; i++) {
-            localStorage.getItem("searchHistory");
+//         function searchHistory() {
 
+/*GET LAT AND LONG OF CITIES from THE WEATHER DATA on 97*/
+//this is saying  that  we want to  search the weather using the city name we will connect to the open weather  api and the  fetch (a promise) will return the information/
+function searchWeather(event) {
+    event.preventDefault();
+    const cityName = searchInput.value;
+    const apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
+    fetch(apiURL)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
 
-            function searchHistory() {
-
-                /*GET LAT AND LONG OF CITIES from THE WEATHER DATA on 97*/
-                //this is saying  that  we want to  search the weather using the city name we will connect to the open weather  api and the  fetch (a promise) will return the information/
-                function searchWeather() {
-                    const cityName = searchInput.value;
-                    const apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
-                    fetch(apiURL)
-                        .then(function (response) {
-                            return response.json()
-                        })
-                        .then(function (data) {
-                            console.log(data);
-
-                        })
-                }
-
+        })
+}
+function fiveDayForecast(lat,lon) {
+    const apiURL = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=" + apiKey;
+}
                 // LOCAL STORAGE
-                localStorage.setItem("search", JSON.stringify(searchHistory));
+                // localStorage.setItem("search", JSON.stringify(searchHistory));
 
                 // CALLS
-                searchHistory();
+                // searchHistory();
 
 
